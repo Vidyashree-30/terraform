9b06c4993f0a0c43c5f6333a580a5aa9d119592d
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-29 19:00:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resourec "local_file" "filename" {
+      filename = "file1.txt"
+      content = "this is from github"
+      }
+resourec "local_file" "filename" {
+      filename = "file2.txt"
+      content = "this is from github"
+      }      
