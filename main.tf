becbfde2354e1e6b628a63c13cbfdea5e21f5522
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-29 19:04:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resourec "local_file" "filename" {
-      filename = "file1.txt"
-      content = "this is from github"
-      }
-resourec "local_file" "filename" {
-      filename = "file2.txt"
-      content = "this is from github"
-      }      
+resource "local_file" "file1" {
+  filename = "file1.txt"
+  content  = "this is from github"
+}
+
+resource "local_file" "file2" {
+  filename = "file2.txt"
+  content  = "this is from github"
+}
