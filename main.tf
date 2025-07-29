1bc025c3d45315b3bb577b7a878972eaa3353edc
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-29 19:07:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "local_file" "file2" {
   filename = "file2.txt"
   content  = "this is from github"
 }
+resource "local_file" "filename" {
+  filename = "example.txt"
+  content  = "This is a test file"
+}
